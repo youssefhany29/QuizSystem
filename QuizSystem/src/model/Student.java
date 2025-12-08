30ac178f454aae9699dc6af9b251d3303e8078f2
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-08 16:46:03 +0300

    Add Student class with properties and methods

--- QuizSystem/src/model/Student.java
+++ QuizSystem/src/model/Student.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Student {
+
+    private String studentId;
+    private String StudentName;
+
+    public Student(String studentId, String StudentName) {
+        this.studentId = studentId;
+        this.StudentName = StudentName;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getStudentName() {
+        return StudentName;
+    }
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
 }
