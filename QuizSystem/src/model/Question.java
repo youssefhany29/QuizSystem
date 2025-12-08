ca8b1e04c7871444a986710f23b683fe6bc40fab
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-08 16:48:27 +0300

    Refactor Question class to be abstract with properties

--- QuizSystem/src/model/Question.java
+++ QuizSystem/src/model/Question.java
@@ -1,4 +1,32 @@
 package model;
 
-public class Question {
+public abstract class Question {
+    private String text;
+    private int score;
+    private Difficulty difficulty;
+
+    public Question(String text, int score, Difficulty difficulty) {
+        this.text = text;
+        this.score = score;
+        this.difficulty = difficulty;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+    public abstract boolean checkAnswer(String userAnswer);
 }
