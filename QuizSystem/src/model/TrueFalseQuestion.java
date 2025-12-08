83b366680e23f364b7f283cd4ce869a138d24d7b
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-08 16:47:40 +0300

    Implement TrueFalseQuestion class with answer checking

--- QuizSystem/src/model/TrueFalseQuestion.java
+++ QuizSystem/src/model/TrueFalseQuestion.java
@@ -1,4 +1,35 @@
 package model;
 
-public class TrueFalseQuestion {
+public class TrueFalseQuestion extends Question{
+
+    private final boolean correctAnswer;
+
+    public TrueFalseQuestion(String text, int score, Difficulty difficulty, boolean correctAnswer){
+        super(text,score,difficulty);
+        this.correctAnswer = correctAnswer;
+    }
+
+    public boolean getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    @Override
+    public boolean checkAnswer(String userAnswer) {
+        if (userAnswer == null ) {
+            return false;
+        }
+        String normalized = userAnswer.trim().toLowerCase();
+
+        boolean userChoice;
+        if (normalized.equals("true") || normalized.equals("t")
+                || normalized.equals("yes") || normalized.equals("y")){
+            userChoice = true;
+        } else if (normalized.equals("false") || normalized.equals("f")
+                || normalized.equals("no") || normalized.equals("n")) {
+            userChoice = false;
+        } else  {
+            userChoice = false;
+        }
+        return userChoice ==  correctAnswer;
+    }
 }
