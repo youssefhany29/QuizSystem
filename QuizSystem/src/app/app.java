18adbfbb35d09b95a62c2da0b1434ff1a429e4db
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-15 02:01:20 +0300

    Refactor app class to Main and add quiz functionality

--- QuizSystem/src/app/app.java
+++ QuizSystem/src/app/app.java
@@ -1,4 +1,22 @@
 package app;
 
-public class app {
+import model.*;
+import service.*;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Question> questions = QuestionLoader.loadFromFile("questions.txt");
+
+        Quiz quiz = new Quiz(questions, 30); // 30 sec timer
+
+        QuizEngine engine = new QuizEngine(quiz);
+
+        engine.startWithTimer();
+
+        System.out.println("\nYour final score: " + quiz.calculateScore());
+    }
 }
