f78f8cf1dbb2cb00beac68f4bcbff94c5fc37072
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-15 02:00:35 +0300

    Add QuizEngine class with quiz management features
    Implemented the QuizEngine class to manage quiz functionality, including question shuffling and user interaction.

--- QuizSystem/src/service/QuizEngine.java
+++ QuizSystem/src/service/QuizEngine.java
@@ -1,4 +1,36 @@
 package service;
 
+import model.Question;
+import model.Quiz;
+import java.util.List;
+import java.util.Scanner;
+
 public class QuizEngine {
+
+    private Quiz quiz;
+    private Scanner scanner = new Scanner(System.in);
+
+    public QuizEngine(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public void start() {
+        quiz.shuffleQuestions();
+
+        List<Question> questions = quiz.getQuestions();
+
+        System.out.println("Quiz Started...");
+        System.out.println("------------------");
+
+        for (Question q : questions) {
+            System.out.println("\nQuestion: " + q.getText());
+            System.out.print("Your answer: ");
+
+            String userAnswer = scanner.nextLine();
+
+            if (q.checkAnswer(userAnswer)) {
+                quiz.addToScore(q.getScore());
+            }
+        }
+    }
 }
